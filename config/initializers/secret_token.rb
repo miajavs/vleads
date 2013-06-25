6621674163a33c84101d0f5b3c87a2daae627f87
Mia Javier <miajavs@gmail.com>
2013-06-24 17:01:27 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vleads::Application.config.secret_token = '9ab80e5fe7e2664f806082c2e59e956685963818cb031f2f1ca34defe2bf7d876c50c08a7d90ee3c4e5f640350b4ef2b2d554578e40db362a93328aed895dda6'
